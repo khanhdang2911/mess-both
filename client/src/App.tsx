28fa2526e414850a2dce34e844435001f7566664
khanhdang2911 <khanhdang3152@gmail.com>
2025-01-14 20:36:11 +0700

    init project

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'flowbite-react'
+function App() {
+  return (
+    <div className='text-center text-red-500'>
+      App
+      <Button>Click me!</Button>
+    </div>
+  )
+}
+
+export default App
