15bc9f1344e528b4864606220a8c8a949a9450bb
khanhdang2911 <khanhdang3152@gmail.com>
2025-01-16 20:10:27 +0700

    feat: handle refresh token

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,10 +3,19 @@ import cors from 'cors'
 import mongoInstance from './dbs/mongo.init'
 import { handleError, handleNotFound } from './middlewares/handleError'
 import router from './routes'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: process.env.FE_URL, // Frontend URL
+    credentials: true // Allow cookies
+  })
+)
 app.use('/', router)
 // eslint-disable-next-line @typescript-eslint/no-unused-expressions
 mongoInstance
--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,11 +1,11 @@
-import { StatusCodes } from 'http-status-codes'
+import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 import ErrorResponse from '~/core/error.response'
 import { IUser, User } from '~/models/user.model'
 import { registerValidation } from '~/validations/auth.validation'
 import dotenv from 'dotenv'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-
+import randtoken from 'rand-token'
 dotenv.config()
 
 const hashPassword = async (password: string) => {
@@ -13,22 +13,44 @@ const hashPassword = async (password: string) => {
   return await bcrypt.hash(password, salt)
 }
 
-const generateAccessToken = async (payload: object) => {
+const generateToken = async (payload: object) => {
   const acccessToken = await jwt.sign(payload, process.env.SECRET_PASSWORD!, { expiresIn: process.env.EXPIRE_IN })
   return acccessToken
 }
 
+const validateToken = async (token: string) => {
+  try {
+    const decoded = await jwt.verify(token, process.env.SECRET_PASSWORD!)
+    return decoded
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (e) {
+    throw new ErrorResponse(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED)
+  }
+}
+
+const validateTokenV2 = async (token: string) => {
+  try {
+    const decoded = await jwt.verify(token, process.env.SECRET_PASSWORD!, { ignoreExpiration: true })
+    return decoded
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (e) {
+    throw new ErrorResponse(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED)
+  }
+}
 const loginService = async (email: string, password: string) => {
   const user = await User.findOne({ email })
   if (!user) throw new ErrorResponse(StatusCodes.BAD_REQUEST, 'Email or password is wrong')
   const validPassword = await bcrypt.compare(password, user.password)
   if (!validPassword) throw new ErrorResponse(StatusCodes.BAD_REQUEST, 'Email or password is wrong')
-  const accessToken = await generateAccessToken({
+  const accessToken = await generateToken({
     id: user._id,
     email: user.email,
     firstname: user.firstname,
     lastname: user.lastname
   })
+  const refreshToken = randtoken.generate(Number(process.env.JWT_REFRESH_TOKEN_SIZE) || 64)
+  user.refreshToken = refreshToken
+  await user.save()
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { password: _, ...userWithoutPassword } = user.toObject()
@@ -48,4 +70,15 @@ const registerService = async (data: IUser) => {
   return newUser
 }
 
-export { loginService, registerService }
+const generateRefreshTokenService = async (refreshToken: string, userId: string) => {
+  const user = await User.findById(userId)
+  if (!user) throw new ErrorResponse(StatusCodes.BAD_REQUEST, 'User not found')
+  if (user.refreshToken !== refreshToken) throw new ErrorResponse(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED)
+  const payload = { id: user._id, email: user.email, firstname: user.firstname, lastname: user.lastname }
+  const newAccessToken = await generateToken(payload)
+  return {
+    ...payload,
+    accessToken: newAccessToken
+  }
+}
+export { loginService, registerService, validateToken, validateTokenV2, generateToken, generateRefreshTokenService }
