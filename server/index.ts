872ae19c71d6840b03e2ecabb2f61a24f93c3084
khanhdang2911 <khanhdang3152@gmail.com>
2025-01-16 17:07:17 +0700

    feat: implement login/register api with handle error

--- server/index.ts
+++ server/index.ts
@@ -4,10 +4,6 @@ dotenv.config()
 
 const PORT = process.env.PORT || 8999
 
-app.use('/', (req, res) => {
-  res.send('Hello World')
-})
-
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
---
+++ server/src/core/error.response.ts
@@ -0,0 +1,11 @@
+class ErrorResponse extends Error {
+  constructor(
+    public statusCode: number,
+    public message: string
+  ) {
+    super(message)
+    this.statusCode = statusCode
+  }
+}
+
+export default ErrorResponse
